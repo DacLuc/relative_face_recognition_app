50a8a700fab06da3ec574f80366bb112071fbb3c
DacLuc <dacluc1603@gmail.com>
2024-01-15 10:15:37 +0700

    old

--- client/main.py
+++ client/main.py
@@ -1,8 +1,9 @@
 from PyQt6 import QtCore, QtWidgets, QtQuick
 import sys
-import home_page, signup_ui, login_ui, main_win, found_users, user_info, credit_ui
+import home_page, signup_ui, login_ui, main_win, found_users, user_info, credit_ui, signup_ui, check_signup, login_ui, check_login
 from controllers.auth import UserAuthController
 
+from models.user_validators import *
 user_auth_controller = UserAuthController()
 
 
@@ -26,7 +27,8 @@ def home_page_ui():
 
 def signup_ui_load():
     global ui
-    ui = signup_ui.Ui_Sign_Up_Page(user_auth_controller)
+    # ui = signup_ui.Ui_Sign_Up_Page(user_auth_controller)
+    ui = signup_ui.Ui_Sign_Up_Page()
     ui.setupUi(Mainwindow)
     ui.cancel_button.clicked.connect(home_page_ui)
     ui.apply_button.clicked.connect(login_ui_load)
@@ -35,7 +37,8 @@ def signup_ui_load():
 
 def login_ui_load():
     global ui
-    ui = login_ui.Ui_Sign_In_Page(user_auth_controller)
+    # ui = login_ui.Ui_Sign_In_Page(user_auth_controller)
+    ui = login_ui.Ui_Sign_In_Page()
     ui.setupUi(Mainwindow)
     ui.cancel_button.clicked.connect(home_page_ui)
     ui.apply_button.clicked.connect(main_win_ui)
@@ -75,6 +78,27 @@ def found_users_ui():
     ui.setupUi(Mainwindow)
     ui.cancel_button.clicked.connect(main_win_ui)
     Mainwindow.show()
+
+
+def check_signup_ui(username, email, password, user_auth_controller: UserAuthController):
+    global ui
+    dlg = QtWidgets.QDialog()
+    ui = check_signup.Ui_check_signup()
+    
+    ui.setupUi(dlg)
+    dlg.exec()
+    # print("===Register btn clicked!")
+    # username = self.account_name.text()
+    # email = self.account_email.text()
+    # password = self.account_password.text()
+    
+    new_user = UserSignUp(
+        username=username, email=email, password=password)
+    # if new_user:
+    # user_auth_controller.register(username, email, password)
+    ui.exit_button.clicked.connect(check_signup_ui)
+
+    # return ui.exit_button.clicked.connect(check_signup)
 
 
 home_page_ui()
